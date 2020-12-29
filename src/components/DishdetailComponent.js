6b4153505330e03447b7a7a2ba4ca55b157cf028
Adeeba-Saleem <adeeadee2345@gmail.com>
2020-12-29 11:52:28 +0530

    adding container

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -23,13 +23,15 @@ class  DishDetail extends Component {
     render(){
 
         return(
+            <div className className="container">
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
-                {this.renderDish(this.props.selectedDish)}
+                {this.renderDish(this.props.dish)}
                 </div>
                 <div className=" col-12 col-md-5 m-1">
                 </div>
 
+            </div>
             </div>
         );
     }
